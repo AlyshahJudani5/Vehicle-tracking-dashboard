db43180ab1783ee00d99e27beb561b3de0947658
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2024-10-31 10:47:52 +0500

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,6 @@ export default function Dashboard() {
             <VehicleList
               onVehicleSelect={setSelectedVehicleId}
               selectedVehicleId={selectedVehicleId}
-              closePanel={setIsPanelOpen}
             />
             {selectedVehicleId && (
               <VehicleDetailPanel
